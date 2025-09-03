35806464451e917ad3c92ab1813506a26c1551e9
Faris Hijazi <theefaris@gmail.com>
2025-09-03 22:45:14 +0300

    feat: Restore Essa Real Estate Arabic content

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,7 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
-  locale: 'en',
+  site_name: 'Essa Real Estate',
+  title: 'عقارات عصّا - استثمار عقاري مضمون',
+  description:
+    'منصة الاستثمار العقاري المجزّأ في السعودية. استثمر من 100,000 ريال واحصل على عوائد 18% في 16 شهر',
+  locale: 'ar',
 };
